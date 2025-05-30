6d69bcad256755ece2a82759d991f77ad4dc3f85
pasqualedimaro <pakydm15@gmail.com>
2025-05-30 11:56:22 +0200

    create sum variable

---
+++ js-snacks-3/main.js
@@ -0,0 +1,5 @@
+/*Il software deve chiedere per 10 volte in dei prompt all'utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+
+// creo la variabile per la somma
+let somma = 0;
