451d6fcb380c650722dc9efce7e681c687c0bcc6
pasqualedimaro <pakydm15@gmail.com>
2025-05-30 12:03:42 +0200

    add for cicle and stamp

--- js-snacks-3/main.js
+++ js-snacks-3/main.js
@@ -3,3 +3,12 @@ Il programma stampa la somma di tutti i numeri inseriti.*/
 
 // creo la variabile per la somma
 let somma = 0;
+
+//apro un ciclo for e chiedo i 10 numeri all'utentew
+
+for (let i = 1; i <= 10; i++) {
+    const numero = parseFloat(prompt("Inserisci il " + i + " numero:"));
+    somma = somma + numero
+}
+
+console.log("La somma di tutti i numeri inseriti è:", somma);
