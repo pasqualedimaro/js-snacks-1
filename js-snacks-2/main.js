458ca023f8c2512becefb2d69e88a62917ce50c1
pasqualedimaro <pakydm15@gmail.com>
2025-05-30 11:30:41 +0200

    create variables

---
+++ js-snacks-2/main.js
@@ -0,0 +1,10 @@
+/*L'utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+
+// creo le due variabili
+const primaParola = prompt("Inserisci la prima parola:");
+const secondaParola = prompt("Inserisci la seconda parola:");
+
+//Verifico quale sia più corta e più lunga, poi le stampo nell'ordine richiesto
+
