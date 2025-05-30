c5af83d3106f960e9f3964141b2c2d672d692787
pasqualedimaro <pakydm15@gmail.com>
2025-05-30 12:34:20 +0200

    for cicle and stamp

--- js-snacks-4/main.js
+++ js-snacks-4/main.js
@@ -6,3 +6,13 @@ se è dispari inseriscilo nell’array.*/
 
 let numeriDispari = [];
 
+// Ciclo for che chiede 6 numeri all'utente
+
+for (let i = 1; i <= 6; i++) {
+    const numero = parseInt(prompt("Inserisci il " + i + "° numero:"));
+    if (numero % 2 !== 0) {
+        numeriDispari.push(numero);
+    }
+} 
+
+console.log("Numeri dispari inseriti:", numeriDispari);
